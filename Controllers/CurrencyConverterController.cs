b7fd2c320d697d93c76457de4c594f8110ce67ee
Cezary <cez94k@gmail.com>
2020-06-22 22:08:36 +0200

    Refactor (Currencies service)

--- Controllers/CurrencyConverterController.cs
+++ Controllers/CurrencyConverterController.cs
@@ -21,45 +21,13 @@ namespace Currency_Conv_Test.Controllers
             _logger = logger;
         }
 
-        static async Task<string> GetCurrencies()
-        {
-            var httpClient = HttpClientFactory.Create();
-
-            var url = "http://api.nbp.pl/api/exchangerates/tables/a/";
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
-
-            if(httpResponseMessage.StatusCode == HttpStatusCode.OK)
-            {
-                var content = httpResponseMessage.Content;
-                var data = await content.ReadAsStringAsync();
-                return data;
-            }
-
-            return httpResponseMessage.StatusCode.ToString();
-        }
-
         [HttpGet]
         public ContentResult Get()
         {
-            var data = GetCurrencies().Result;
-            var currencies = JsonConvert.DeserializeObject<IEnumerable<Currencies>>(data);
-
-            var tableA = currencies.First();
-            var rates = tableA.Rates;
-
-            var availableCurrencies = new List<string>();
-
-            foreach(var rate in rates)
-            {
-                availableCurrencies.Add($"{rate.Currency}, code: {rate.Code}, mid: {rate.Mid}");
-            }
-
-            var result = String.Join("</br>", availableCurrencies.Take(5).ToArray());
-
             return new ContentResult
             {
                 ContentType = "text/html; charset=utf-8",
-                Content = result,
+                Content = CurrenciesService.GetAvailableCurrencies()
             };
         }
 
---
+++ CurrenciesService.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Currency_Conv_Test
+{
+    public static class CurrenciesService
+    {
+        public static async Task<string> GetCurrencies()
+        {
+            var httpClient = HttpClientFactory.Create();
+
+            var url = "http://api.nbp.pl/api/exchangerates/tables/a/";
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+
+            if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+            {
+                var content = httpResponseMessage.Content;
+                var data = await content.ReadAsStringAsync();
+                return data;
+            }
+
+            return httpResponseMessage.StatusCode.ToString();
+        }
+
+        public static string GetAvailableCurrencies()
+        {
+            var data = CurrenciesService.GetCurrencies().Result;
+            var currencies = JsonConvert.DeserializeObject<IEnumerable<Currencies>>(data);
+
+            var tableA = currencies.First();
+            var rates = tableA.Rates;
+
+            var availableCurrencies = new List<string>();
+
+            foreach (var rate in rates)
+            {
+                availableCurrencies.Add($"{rate.Currency}, code: {rate.Code}, mid: {rate.Mid}");
+            }
+
+            return String.Join("</br>", availableCurrencies.ToArray());
+        }
+    }
+}
