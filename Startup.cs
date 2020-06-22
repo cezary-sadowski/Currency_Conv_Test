ee231cb01919ce4efb5dfa1daa53b0f55df6017b
Cezary <cez94k@gmail.com>
2020-06-23 00:13:22 +0200

    Swagger implementation

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Currency_Conv_Test
 {
@@ -18,6 +19,10 @@ namespace Currency_Conv_Test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -29,6 +34,13 @@ namespace Currency_Conv_Test
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -37,6 +49,8 @@ namespace Currency_Conv_Test
             {
                 endpoints.MapControllers();
             });
+
+            
         }
     }
 }
