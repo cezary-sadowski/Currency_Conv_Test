318cf1ce44c82edd3de6327c2c4cea0b9756956d
Cezary <cez94k@gmail.com>
2020-06-23 00:01:05 +0200

    refactor

--- Controllers/CurrencyConverterController.cs
+++ Controllers/CurrencyConverterController.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Currency_Conv_Test.Controllers
 {
