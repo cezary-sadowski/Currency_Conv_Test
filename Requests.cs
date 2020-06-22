25d0607c7347ec6818e4ff9e4a16575f91cf9bee
Cezary <cez94k@gmail.com>
2020-06-23 01:38:41 +0200

    Entity Framework core + db connection

---
+++ Requests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Currency_Conv_Test
+{
+    public class Requests
+    {
+        [Key]
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +20,9 @@ namespace Currency_Conv_Test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DBConnection>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
