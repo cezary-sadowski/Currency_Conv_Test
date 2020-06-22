69711e0cec68c31068188f03d7e0ca3438e51e2f
Cezary <cez94k@gmail.com>
2020-06-22 20:06:35 +0200

    Currency Converter DTO

---
+++ CurrencyConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Currency_Conv_Test
+{
+    public partial class CurrencyConverter
+    {
+        [JsonProperty("table")]
+        public string Table { get; set; }
+
+        [JsonProperty("no")]
+        public string No { get; set; }
+
+        [JsonProperty("effectiveDate")]
+        public DateTimeOffset EffectiveDate { get; set; }
+
+        [JsonProperty("rates")]
+        public Rate[] Rates { get; set; }
+    }
+
+    public partial class Rate
+    {
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("mid")]
+        public double Mid { get; set; }
+    }
+}
